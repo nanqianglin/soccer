eeea4a65f03625749b73edabac81347aa1db259c
nanqiang <nanqianglin@gmail.com>
2022-08-14 12:51:52 +0800

    fix: render the game status

--- src/components/GameDetails.tsx
+++ src/components/GameDetails.tsx
@@ -15,7 +15,7 @@ import {
 import { ethers } from "ethers";
 import useGameExtraInfo from "hooks/useGameExtraInfo";
 import moment from "moment";
-import React from "react";
+import React, { useCallback } from "react";
 import { useAccount, useBlockNumber } from "wagmi";
 import ApproveOrReject from "./ApproveOrReject";
 import PlayForm from "./PlayForm";
@@ -57,90 +57,148 @@ const GameDetails = ({ data }: Props) => {
 	const canFinish = quorum && gameStatus.approvers.gte(quorum);
 	const shouldPunish = rejectQuorum && gameStatus.rejecters.gte(rejectQuorum);
 
+	const renderGameOptions = useCallback(() => {
+		if (isOwner && !isRevealed) return;
+
+		return (
+			<RadioGroup mt={10}>
+				{options.map((item: any) => (
+					<Radio
+						key={item.key}
+						value={item.value}
+						mr={10}
+						colorScheme='purple'
+						isDisabled
+					>
+						{item.label}
+					</Radio>
+				))}
+			</RadioGroup>
+		);
+	}, [isOwner, isRevealed, options]);
+
+	const renderStatusSection = useCallback(() => {
+		// can play game
+		if (!isExpired && !isFinished && !isRevealed) {
+			return <PlayForm id={data.id.toNumber()} gameInfo={gameInfo} />;
+		}
+		// can reveal
+		if (isExpired && !isRevealed && !isFinished) {
+			return (
+				<Box mt={4}>
+					{/* Reveal Button */}
+					{isOwner ? (
+						<RevealForm id={data.id} gameInfo={gameInfo} />
+					) : (
+						<Alert status='warning'>
+							<AlertIcon />
+							This game is expired and waiting for reveal.
+						</Alert>
+					)}
+				</Box>
+			);
+		}
+		// the game is finished
+		if (isFinished) {
+			return (
+				<Box mt={4}>
+					<Box mt={6}>
+						And the correct answer is{" "}
+						<Text color='red' fontWeight='bold'>
+							{correctAnswer !== undefined ? options[correctAnswer].label : ""}
+						</Text>
+					</Box>
+					<Alert status='warning' mt={4}>
+						<AlertIcon />
+						This game is finished.
+					</Alert>
+				</Box>
+			);
+		}
+		// can punish
+		if (shouldPunish) {
+			return (
+				<Box mt={4}>
+					<Box mt={6}>
+						And the correct answer is{" "}
+						<Text color='red' fontWeight='bold'>
+							{correctAnswer !== undefined ? options[correctAnswer].label : ""}
+						</Text>
+					</Box>
+					<Text fontSize='small' my={2}>
+						This game is dishonest, as it got some rejecters. Please punish
+						he/she, thank you!
+					</Text>
+					<Button colorScheme='red'>Punish the game owner</Button>
+				</Box>
+			);
+		}
+		// can finish
+		if (canFinish) {
+			return (
+				<Box mt={4}>
+					<Box mt={6}>
+						And the correct answer is{" "}
+						<Text color='red' fontWeight='bold'>
+							{correctAnswer !== undefined ? options[correctAnswer].label : ""}
+						</Text>
+					</Box>
+					<Text fontSize='small' my={2}>
+						This game can be finish, as it got enough approvers. Please finish
+						it, thank you!
+					</Text>
+					<Button colorScheme='purple'>Finish the game</Button>
+				</Box>
+			);
+		}
+		// can approve
+		if (isRevealed && !isFinished) {
+			return (
+				<Box mt={4}>
+					<Alert status='warning'>
+						<AlertIcon />
+						This game is revealed and waiting for approval.
+					</Alert>
+					<Box mt={6}>
+						And the correct answer is{" "}
+						<Text color='red' fontWeight='bold'>
+							{correctAnswer !== undefined ? options[correctAnswer].label : ""}
+						</Text>
+					</Box>
+					{/* Approve Button */}
+					{/* Reject Button */}
+					{isApprover && (
+						<ApproveOrReject
+							id={data.id}
+							isApprovedOrRejected={Boolean(isApprovedOrRejected)}
+						/>
+					)}
+				</Box>
+			);
+		}
+	}, [
+		canFinish,
+		correctAnswer,
+		data.id,
+		gameInfo,
+		isApprovedOrRejected,
+		isApprover,
+		isExpired,
+		isFinished,
+		isOwner,
+		isRevealed,
+		options,
+		shouldPunish,
+	]);
+
 	return (
 		<Box border='1px solid' borderColor='gray.200' borderRadius={8} p={10}>
 			<Heading as='h5'>{gameInfo.title}</Heading>
 			<Text fontSize='lg' mt={4}>
 				{gameInfo.description}
 			</Text>
-			{(isExpired || isFinished) && (!isOwner || isRevealed) && (
-				<RadioGroup mt={10}>
-					{options.map((item: any) => (
-						<Radio
-							key={item.key}
-							value={item.value}
-							mr={10}
-							colorScheme='purple'
-							isDisabled
-						>
-							{item.label}
-						</Radio>
-					))}
-				</RadioGroup>
-			)}
-			<Box mt={10}>
-				{!isExpired && !isFinished && !isRevealed && (
-					<PlayForm id={data.id.toNumber()} gameInfo={gameInfo} />
-				)}
-				{/* Finished status */}
-				{isFinished && (
-					<Alert status='warning' mt={4}>
-						<AlertIcon />
-						This game is finished.
-					</Alert>
-				)}
-				{/* Expired status */}
-				{isExpired && !isRevealed && (
-					<Box mt={4}>
-						{/* Reveal Button */}
-						{isOwner ? (
-							<RevealForm id={data.id} gameInfo={gameInfo} />
-						) : (
-							<Alert status='warning'>
-								<AlertIcon />
-								This game is expired and waiting for reveal.
-							</Alert>
-						)}
-					</Box>
-				)}
-				{/* Revealed status */}
-				{isRevealed && !isFinished && (
-					<Box mt={4}>
-						<Alert status='warning'>
-							<AlertIcon />
-							This game is revealed and waiting for approval.
-						</Alert>
-						<Box mt={6}>
-							And the correct answer is{" "}
-							<Text color='red' fontWeight='bold'>
-								{correctAnswer !== undefined
-									? options[correctAnswer].label
-									: ""}
-							</Text>
-						</Box>
-						{/* Approve Button */}
-						{/* Reject Button */}
-						{isApprover && (
-							<ApproveOrReject
-								id={data.id}
-								isApprovedOrRejected={Boolean(isApprovedOrRejected)}
-							/>
-						)}
-					</Box>
-				)}
-				{/* Finish Button */}
-				{canFinish && (
-					<Box mt={4}>
-						<Button colorScheme='purple'>Finish the game</Button>
-					</Box>
-				)}
-				{/* Punish Button */}
-				{shouldPunish && (
-					<Box mt={4}>
-						<Button colorScheme='purple'>Punish the game owner</Button>
-					</Box>
-				)}
-			</Box>
+			{renderGameOptions()}
+			<Box mt={10}>{renderStatusSection()}</Box>
 
 			{!isExpired && (
 				<Text fontSize='sm' mt={4} color='red'>
